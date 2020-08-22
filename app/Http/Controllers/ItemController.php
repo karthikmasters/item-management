e5d658095b3ac017858e4e66d4dd68b38509e1ce
Karthik Marri <kmarri@americanfirstfinance.com>
2020-08-22 22:50:29 +0530

    Adding try catch block

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,30 +4,43 @@ namespace App\Http\Controllers;
 use App\Item;
 
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
 class ItemController extends Controller {
     public function index() {
-        $items = Item::all();
-        $aItems = ['right' => [],'left' => []];
-        foreach ($items as $item) {
-            $aItems[$item->selected == 1 ? 'right' : 'left'][] = ['id' => $item->id, 'name' => $item->name];
+        try {
+            $items = Item::all();
+            $aItems = ['right' => [],'left' => []];
+            foreach ($items as $item) {
+                $aItems[$item->selected == 1 ? 'right' : 'left'][] = ['id' => $item->id, 'name' => $item->name];
+            }
+            return response()->json(['success' => true, 'data' => $aItems]);
+        } catch (\Exception $exception) {
+            return response()->json(['success' => false, 'message' => 'Something went wrong! Unable to get items']);
         }
-        return response()->json(['success' => true, 'data' => $aItems]);
     }
 
     public function save(Request $request) {
-        $itemFound = Item::where('name', $request->item)->count();
-        if ($itemFound > 0) {
-            return response()->json(['success' => true, 'message' => 'Record already exists']);
+        try {
+            $itemFound = Item::where('name', $request->item)->count();
+            if ($itemFound > 0) {
+                return response()->json(['success' => false, 'message' => 'Record already exists']);
+            }
+            $Item = Item::create(['name' => $request->item]);
+            return response()->json(['success' => true, 'message' => 'Record successfully created']);
+        } catch (\Exception $exception) {
+            return response()->json(['success' => false, 'message' => 'Something went wrong! Unable to add this item']);
         }
-        $Item = Item::create(['name' => $request->item]);
-        return response()->json(['success' => true, 'message' => 'Record successfully created']);
     }
 
     public function update(Request $request, $id) {
-        $item = Item::find($id);
-        $item->selected = $request->selected;
-        $item->save();
-        return response()->json(['success' => true, 'data' => []]);
+        try {
+            $item = Item::find($id);
+            $item->selected = $request->selected;
+            $item->save();
+            return response()->json(['success' => true, 'data' => []]);
+        } catch (\Exception $exception) {
+            return response()->json(['success' => false, 'message' => 'Something went wrong! Unable to move this item']);
+        }
     }
 }
